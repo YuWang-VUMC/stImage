8cf2f87b8cee7c80055a74ad05a319537d325e71
YuWang-VUMC <yu.wang.2@vumc.org>
2022-11-01 16:29:06 -0500

    add spark

---
+++ R/doSPARK.R
@@ -0,0 +1,33 @@
+require(SPARK)
+
+doSpark <- function(object,
+                    n.core = 1,
+                    genePercentCut = 0.05,
+                    min_total_counts = 10) {
+  cell_n <- length(Cells(object))
+  counts <- object@assays$Spatial@counts
+  location <- GetTissueCoordinates(object)
+  if(cell_n < 1000){
+    spark <- CreateSPARKObject(counts = counts,
+                               location = location,
+                               percentage = genePercentCut,
+                               min_total_counts = min_total_counts)
+    spark@lib_size <- apply(spark@counts, 2, sum)
+    spark <- spark.vc(spark,
+                      covariates = NULL,
+                      lib_size = spark@lib_size,
+                      num_core = n.core,
+                      verbose = F)
+    spark <- spark.test(spark,
+                        check_positive = T,
+                        verbose = F)
+    result <- spark@res_mtest[,c("combined_pvalue","adjusted_pvalue")]
+  } else {
+    sparkX <- sparkx(counts,
+                     location,
+                     numCores = n.core,
+                     option = "mixture")
+    result <- sparkX$res_mtest
+  }
+  return(result)
+}
